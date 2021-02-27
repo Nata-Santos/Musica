5d919d3f52caec8ff0e923c82d62395750a6e6ba
Natã dos Santos Silva <natan.sfc9@gmail.com>
2021-02-27 00:32:57 -0300

    Exercicio PC1 Musica

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor=ConsoleColor.DarkCyan;
+            Console.WriteLine("Don't Give Up");
+            Console.WriteLine("-------------");
+            Console.ForegroundColor=ConsoleColor.DarkGreen;
+            Console.WriteLine("Bruno Mars");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("When you wanna do something that's new");
+            Console.WriteLine("And it seems really, really hard to do");
+            Console.WriteLine("You feel like quitting, feel ya through");
+            Console.WriteLine("Well I have some advice for you");
+            Console.WriteLine();
+            Console.ForegroundColor=ConsoleColor.DarkRed;
+            Console.WriteLine("Refrão");
+            Console.WriteLine();
+            Console.WriteLine("Don't give up");
+            Console.WriteLine("Keep on tryin'");
+            Console.WriteLine("You're gonna make it");
+            Console.WriteLine("I ain't lyin'");
+            Console.WriteLine("Don't give up, don't ever quit");
+            Console.WriteLine("Try and try and you can do it");
+            Console.WriteLine("Don't give up, yeah");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("If you want to catch a ball");
+            Console.WriteLine("But you're havin' no luck at all");
+            Console.WriteLine("The ball hits your head, it hits your nose");
+            Console.WriteLine("It hits your belly, your chin and toes");
+            Console.WriteLine("Well, try and try and try again");
+            Console.WriteLine("Keep on trying and soon than");
+            Console.WriteLine("You put your hands out in the air");
+            Console.WriteLine("You'll catch the ball");
+            Console.WriteLine("Yes this I swear");
+            Console.ForegroundColor=ConsoleColor.DarkRed;
+            Console.WriteLine();
+            Console.WriteLine("Don't give up");
+            Console.WriteLine("Keep on going");
+            Console.WriteLine("You're on a boat");
+            Console.WriteLine("So keep on rowing");
+            Console.WriteLine("Don't give up, don't ever stop");
+            Console.WriteLine("Try and try and you'll come out on top");
+            Console.WriteLine("Don't give up");
+            Console.WriteLine();
+            Console.ResetColor();
+            Console.WriteLine("You got yourself rollerskates");
+            Console.WriteLine("You put them on and you feel great");
+            Console.WriteLine("You stand up, then you fall");
+            Console.WriteLine("Don't think you can skate at all");
+            Console.WriteLine("You get back up, then you trip");
+            Console.WriteLine("You skip and tip and slip, and flip");
+            Console.WriteLine("You try and try and try some more");
+            Console.WriteLine("Soon you're skating across the floor");
+            Console.WriteLine();
+            Console.ForegroundColor=ConsoleColor.DarkRed;
+            Console.WriteLine("Refrão");
+            Console.WriteLine();
+            Console.WriteLine("Don't give up, keep on moving");
+            Console.WriteLine("You're gonna get there, just keep on grooving");
+            Console.WriteLine("Don't give up, don't pack it in");
+            Console.WriteLine("Try and try, and you'll win");
+            Console.WriteLine("Don't give up, no no no");
+            Console.WriteLine("Don't give up, no no no");
+            Console.WriteLine("Don't give up, no no no");
+            Console.WriteLine("Don't give up, no no");
+            Console.WriteLine("Don't give up!");
+
+
+
+            
+
+
+
+           
+
+
+            
+
+            
+
+        }
+    }
+}
